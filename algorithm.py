5ec174c611ebf41e25f94d8e65d46f2cadbf035b
Daniel-Ros <Danimalau@live.com>
2021-11-13 19:52:37 +0200

    this sim was worst then hell :(

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,14 @@
+from call import MultiCall
+
+
 class Algo:
     def __init__(self, b, c):
         self.building = b
         self.calls = c
 
     def run(self):
+        self.join_calls()
+        print(len(self.calls))
         for c in self.calls:
             min_time = 2147483647
             min_elev = None
@@ -13,3 +18,16 @@ class Algo:
                     min_time = time_wasted
                     min_elev = e
             c.alloc(min_elev)
+
+    def join_calls(self):
+        i = 0
+        while i < len(self.calls) - 2:
+            j = i + 1
+            if abs(self.calls[i].src - self.calls[j].src) < (self.building.max_floor - self.building.min_floor) * 0.1:
+                if self.calls[i].src > self.calls[i].dest and self.calls[j].src > self.calls[j].dest or self.calls[
+                    i].src < self.calls[i].dest and self.calls[j].src < self.calls[j].dest:
+                    multi_call = MultiCall([self.calls[i], self.calls[j]])
+                    self.calls[i] = multi_call
+                    self.calls.remove(self.calls[j])
+                    i += 1
+            i += 1
--- call.py
+++ call.py
@@ -38,3 +38,35 @@ class Call:
 
     def __str__(self):
         return "Elevator call,{},{},{},0,{}\n".format(self.time, self.src, self.dest, self.elev.id)
+
+
+class MultiCall:
+    def __init__(self, calls):
+        self.calls = calls
+        self.src = calls[0].src
+        self.dest = calls[0].dest
+        self.time = math.ceil(float(calls[0].time))
+        self.elev = -1
+        self.state = State.GOING2SRC
+
+    def alloc(self, elev):
+        for c in self.calls:
+            c.alloc(elev)
+
+    def de_alloc(self, elev):
+        for c in self.calls:
+            c.de_alloc(elev)
+
+    def pickup(self):
+        for c in self.calls:
+            c.state = State.GOING2DEST
+
+    def drop_off(self):
+        for c in self.calls:
+            c.state = State.DONE
+
+    def __repr__(self):
+        return "Elevator call,{},{},{},0,{}\n".format(self.time, self.src, self.dest, self.elev.id)
+
+    def __str__(self):
+        return "".join(map(str, self.calls))
